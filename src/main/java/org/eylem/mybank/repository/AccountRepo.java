29674dff55dafa521d26d0997beb8e4152407646
Abdul Kadir <52890922+AbdulKadir100@users.noreply.github.com>
2023-03-07 12:54:10 +0530

    Update AccountRepo.java
    To fetch all the accounts we need this, Althogh its optional.

--- src/main/java/org/eylem/mybank/repository/AccountRepo.java
+++ src/main/java/org/eylem/mybank/repository/AccountRepo.java
@@ -6,9 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
 
 public interface AccountRepo extends CrudRepository<Account, UUID> {
     Account deleteAccountById(UUID id);
+    List<Account> getAllAccounts();
     Optional<Account> findById(UUID id);
     Optional<Account> findByCustomer(Customer customer);
     Optional<Account> getByIban(String iban);
